717cc6275772629c2056cc29eb7bc9fccb4feacd
Jon Manning <jon@secretlab.com.au>
2025-07-20 15:30:13 +1000

    Update internal web view title

--- src/panels/YarnSpinnerGraphView.ts
+++ src/panels/YarnSpinnerGraphView.ts
@@ -287,7 +287,7 @@ export class YarnSpinnerGraphView {
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
           <link rel="stylesheet" type="text/css" href="${stylesUri}">
-          <title>Hello World</title>
+          <title>Yarn Spinner Graph View</title>
         </head>
         <body>
           <div id="root" data-vscode-context='{"preventDefaultContextMenuItems": true}'></div>
