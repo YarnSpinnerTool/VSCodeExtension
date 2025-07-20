a48338b4e958d8c8e70bd13042c6e656f975d45f
Jon Manning <jon@secretlab.com.au>
2025-07-20 18:46:58 +1000

    Add svgr

--- graph-view/vite.config.ts
+++ graph-view/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
+    plugins: [react(), tailwindcss(), svgr()],
     build: {
         outDir: "build",
         rollupOptions: {
